cb0c8bcab27ccd6a72b7377b793d7d67b68ca4da
Ryan Chu <ryanchutx@gmail.com>
2021-02-20 13:35:07 -0600

    first commit for required tasks

---
+++ Flashcards/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Flashcards
+//
+//  Created by Ryan Chu on 2/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var backLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        frontLabel.isHidden = true;
+    }
+    
+}
+
