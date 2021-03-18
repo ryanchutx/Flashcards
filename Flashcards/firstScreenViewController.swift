f499c3660fdb3e5f0806de48e326c0ea7f12807b
Ryan Chu <ryanchutx@gmail.com>
2021-03-17 21:02:49 -0500

    commit for lab 3 required materials

--- Flashcards/firstScreenViewController.swift
+++ Flashcards/firstScreenViewController.swift
@@ -7,6 +7,11 @@
 
 import UIKit
 
+struct Flashcard {
+    var question: String
+    var answer: String
+}
+
 class firstScreenViewController: UIViewController {
 
     // IBOutlets for various components and objects used within the storyboard
@@ -17,6 +22,12 @@ class firstScreenViewController: UIViewController {
     @IBOutlet weak var btnOptionOne: UIButton!
     @IBOutlet weak var btnOptionTwo: UIButton!
     @IBOutlet weak var btnOptionThree: UIButton!
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var flashcards = [Flashcard]()
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +47,15 @@ class firstScreenViewController: UIViewController {
         btnOptionThree.layer.cornerRadius = 10;
         btnOptionThree.layer.borderWidth = 3;
         btnOptionThree.layer.borderColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        
+        readSavedFlashcards()
+        
+        if flashcards.count == 0 {
+            updateFlashcard(question: "Where is the U.S. Capitol Located?", answer: "Washington D.C")
+        } else {
+            updateLabels()
+            updateNextPrevButtons()
+        }
         
     }
 
@@ -60,9 +80,50 @@ class firstScreenViewController: UIViewController {
 
     }
     
+    @IBAction func didTapOnPrev(_ sender: Any) {
+        currentIndex = currentIndex - 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    @IBAction func didTapOnNext(_ sender: Any) {
+        currentIndex = currentIndex + 1
+        updateLabels()
+        updateNextPrevButtons()
+    }
+    
+    func updateLabels() {
+        let currentFlashcard = flashcards[currentIndex]
+        frontLabel.text = currentFlashcard.question
+        backLabel.text = currentFlashcard.answer
+    }
+    
     func updateFlashcard(question: String, answer: String) {
-        frontLabel.text = question
-        backLabel.text = answer
+        let flashcard = Flashcard(question:question, answer:answer)
+        frontLabel.text = flashcard.question
+        backLabel.text = flashcard.answer
+        flashcards.append(flashcard)
+        print("😼 added new flashcard")
+        print("😎 we now have \(flashcards.count) flashcards")
+        currentIndex = flashcards.count - 1
+        print("😱 our current index is \(currentIndex)")
+        updateNextPrevButtons()
+        updateLabels()
+        saveAllFlashcardsToDisk()
+    }
+    
+    func updateNextPrevButtons() {
+        if currentIndex == flashcards.count - 1 {
+            nextButton.isEnabled = false
+        } else {
+            nextButton.isEnabled = true
+        }
+        
+        if currentIndex == 0 {
+            prevButton.isEnabled = false
+        } else {
+            prevButton.isEnabled = true
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -73,6 +134,24 @@ class firstScreenViewController: UIViewController {
         
         creationController.flashcardsController = self
         
+    }
+    
+    func saveAllFlashcardsToDisk() {
+        let dictionaryArray = flashcards.map{(card) -> [String: String] in return ["question": card.question, "answer": card.answer]}
+        
+        UserDefaults.standard.set(dictionaryArray, forKey: "flashcards")
+        
+        print("🎉 flashcards saved to UserDefaults")
+    }
+    
+    func readSavedFlashcards() {
+        if let dictionaryArray = UserDefaults.standard.array(forKey: "flashcards") as? [[String: String]] {
+        
+            let savedCards = dictionaryArray.map { dictionary -> Flashcard in return Flashcard(question: dictionary["question"]!, answer: dictionary["answer"]!)
+            }
+            
+            flashcards.append(contentsOf: savedCards)
+        }
     }
     
 }
